452d2b71849c7a66e7b08a40efd91c0c9052fb3a
petrikpopov <112963523+petrikpopov@users.noreply.github.com>
2023-02-10 16:21:38 +0200

    Add files via upload

---
+++ IndexMatrix.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Indexator10._02._2022Matrix
+{
+    public class IndexMatrix
+    {
+       
+        public int Count_Line { set; get; }
+        public int Count_Column { set; get; }
+        private int[,] mass;
+
+        public IndexMatrix() { }
+
+        public IndexMatrix(int CL, int CC)
+        {
+            Count_Column = CC;
+            Count_Line = CL;
+            mass = new int[Count_Line, Count_Column];
+        }
+
+        public int this[int CL, int CC]
+        {
+            get
+            {
+                if (CL < 0 || CL > mass.GetLength(0))
+                {
+                    throw new Exception("Не коректный индекс:" + CL);
+                }
+                else if (CC < 0 || CC > mass.GetLength(1))
+                {
+                    throw new Exception("Не коректный индекс:" + CC);
+                }
+                
+            }
+            set
+            {
+                if (CL > 0 || CL < mass.GetLength(0))
+                {
+                    CL = value;
+                }
+                else if (CL > 0 || CL < mass.GetLength(1))
+                {
+                    CC = value;
+                }
+                else
+                {
+                    throw new Exception("Не коректный индекс:");
+                }
+
+            }
+            
+
+        }
+
+        /*static IndexMatrix()
+        {
+            Console.Write("Введите кол-во строк в матрице:");
+            Count_Line = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите кол-во столбцов в матрице:");
+            Count_Column = Convert.ToInt32(Console.ReadLine());
+        }*/
+
+        /*public void Mass()
+        {
+            int Max = mass[0, 0];
+            int Min = mass[0, 0];
+            int j = 0;
+            int i = 0;
+            Random R = new Random();
+            for (i = 0; i < Count_Line; i++)
+            {
+                for (j = 0; j < Count_Column; j++)
+                {
+                    mass[i, j] = R.Next(1, 30);
+                    Console.Write("{0}\t", mass[i, j]);
+                    if (mass[i, j] > Max)
+                    {
+                        Max = mass[i, j];
+                    }
+                    if (mass[i, j] < Min)
+                    {
+                        Min = mass[i, j];
+                    }
+                }
+
+                Console.WriteLine();
+            }
+            Console.WriteLine("Максимальный элимент = " + Max);
+            Console.WriteLine("Минимальный элимент = " + Min);
+        }*/
+    }
+}   
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Indexator10._02._2022Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+
+            IndexMatrix matrix = new IndexMatrix(3, 4);
+            for (int i = 0; i < matrix.Count_Line; i++)
+            {
+                for (int j = 0; j < matrix.Count_Column; j++)
+                {
+                    try
+                    {
+                        matrix[i, j] = i + j;
+                        Console.WriteLine(matrix[i, j] + "\t");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+
+                }            
+
+
+
+            }
+            Console.WriteLine();
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+
+}
+
+
